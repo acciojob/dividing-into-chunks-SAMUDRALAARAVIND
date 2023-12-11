2dcdf8e2929f9c97adf4aaa72248c7853fb7a689
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-12-11 23:05:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2]; // n = 5
 		//                           i
 
 const divide = (arr, n) => {
+	// corner case 
+	if(arr.length === 0) return [] ;
+
+	
 	let trackingSum = 0 , ans = [] , subArr = [] ;
 	// ans = [ [1, 2] , [ 3 ], [ 4, 1, 0 ], [ 2, 2 ] ] 
 	// subArr = [ 2, 2 ], trackingSum = 4
@@ -19,10 +23,10 @@ const divide = (arr, n) => {
 		}
 	}
 
-	ans.push(subArr);
+	ans.push(subArr); // [ [] ]
 
 	return ans ; // [ [1, 2] , [ 3 ], [ 4, 1, 0 ], [ 2, 2 ] ] 
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
